b7e790666fc0e821de331b0bcf7bca0a1eb0c7d8
kio948a <92074037+kio948a@users.noreply.github.com>
2024-01-31 17:24:57 -0600

    Update keys.js

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'carniceria'
+        host: 'ls-6e7036dcedceb1a18c62465d3d6f5deb0d9253a5.cfwya5zlkfpi.us-east-2.rds.amazonaws.com',
+        user: 'dbmasteruser',
+        password: 'aguacate',
+        database: 'carniceria2'
     }
 };
